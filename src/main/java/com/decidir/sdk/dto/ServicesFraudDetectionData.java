109f4bc42aa68be20eccbdbde3a8e8a1bd64c830
hdillon <dillonhoraciodavid@gmail.com>
2017-08-25 17:08:58 -0300

    Vertical Services

---
+++ src/main/java/com/decidir/sdk/dto/ServicesFraudDetectionData.java
@@ -0,0 +1,18 @@
+package com.decidir.sdk.dto;
+
+/**
+ * Services specific Fraud Detection Data DTO
+ */
+public class ServicesFraudDetectionData extends FraudDetectionDataRequest {
+
+    private ServicesTransactionData services_transaction_data;
+
+    public ServicesTransactionData getServices_transaction_data() {
+        return services_transaction_data;
+    }
+
+    public void setServices_transaction_data(ServicesTransactionData services_transaction_data) {
+        this.services_transaction_data = services_transaction_data;
+    }
+
+}
