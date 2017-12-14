4b3939ae55b06ddfa2f0868f3623623618b796fc
Ignacio Valek <ignacio.valek@redb.ee>
2017-12-14 16:55:08 -0300

    Fix: Error al deserialzar json con propiedades desconocidas

--- src/main/java/com/decidir/sdk/configuration/DecidirConfiguration.java
+++ src/main/java/com/decidir/sdk/configuration/DecidirConfiguration.java
@@ -5,8 +5,13 @@ import java.text.SimpleDateFormat;
 import java.util.TimeZone;
 import java.util.concurrent.TimeUnit;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.fasterxml.jackson.databind.deser.DeserializationProblemHandler;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -47,7 +52,12 @@ public class DecidirConfiguration {
             }
         });
         
-        ObjectMapper jacksonConverter = new ObjectMapper();
+        ObjectMapper jacksonConverter = new ObjectMapper().addHandler(new DeserializationProblemHandler() {
+            @Override
+            public boolean handleUnknownProperty(DeserializationContext ctxt, JsonParser jp, JsonDeserializer<?> deserializer, Object beanOrClass, String propertyName) throws IOException, JsonProcessingException {
+                return true;
+            }
+        });
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
         dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
 		jacksonConverter.setDateFormat(dateFormat);
