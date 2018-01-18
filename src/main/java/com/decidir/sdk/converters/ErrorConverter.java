dc442bff237e2465a4119936d3156526134bcefb
Ignacio Valek <ignacio.valek@redb.ee>
2018-01-18 16:11:10 -0300

    Fix: Error al deseriailzar errores

--- src/main/java/com/decidir/sdk/converters/ErrorConverter.java
+++ src/main/java/com/decidir/sdk/converters/ErrorConverter.java
@@ -1,23 +1,20 @@
 package com.decidir.sdk.converters;
 
-import java.io.IOException;
-
-import com.decidir.sdk.dto.DecidirResponse;
 import com.decidir.sdk.exceptions.DecidirError;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import com.decidir.sdk.dto.DecidirResponse;
 import retrofit2.Response;
+
+import java.io.IOException;
 
 /**
  * Created by biandra on 04/08/16.
  */
-public class ErrorConverter {
+public class ErrorConverter extends DecidirConverter {
 
     public DecidirResponse<DecidirError> convert(Response response) throws IOException {
-        ObjectMapper objectMapper = new ObjectMapper();
         DecidirResponse<DecidirError> dResponse = new DecidirResponse();
         dResponse.setStatus(response.code());
-        dResponse.setResult(objectMapper.readValue(response.errorBody().string(), DecidirError.class));
+        dResponse.setResult(super.convert(response.errorBody().bytes(), DecidirError.class));
         dResponse.setMessage(response.message());
         return dResponse;
     }
