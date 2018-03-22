abdebd4be9130ef30b191df03dfb126a60c8e7be
Juan José Mejía Rodrígue <jmejia@prismamp.com>
2018-03-22 15:04:00 -0300

    DECD-3068: Fix catch de exceptions que no populaban, y agregado estado CANCELLED para estado de devoluciones

--- src/main/java/com/decidir/sdk/services/RefundsService.java
+++ src/main/java/com/decidir/sdk/services/RefundsService.java
@@ -1,23 +1,22 @@
 package com.decidir.sdk.services;
 
-import com.decidir.sdk.converters.ErrorConverter;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+
 import com.decidir.sdk.converters.PaymentConverter;
-import com.decidir.sdk.dto.*;
+import com.decidir.sdk.dto.DecidirResponse;
 import com.decidir.sdk.dto.annullment.AnnulRefundResponse;
 import com.decidir.sdk.dto.refunds.RefundMPOSPayment;
 import com.decidir.sdk.dto.refunds.RefundPayment;
 import com.decidir.sdk.dto.refunds.RefundPaymentHistoryResponse;
 import com.decidir.sdk.dto.refunds.RefundPaymentResponse;
 import com.decidir.sdk.exceptions.DecidirError;
-import com.decidir.sdk.exceptions.responses.AnnulRefundException;
 import com.decidir.sdk.exceptions.DecidirException;
+import com.decidir.sdk.exceptions.responses.AnnulRefundException;
 import com.decidir.sdk.exceptions.responses.RefundException;
 import com.decidir.sdk.resources.RefundApi;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
+
 import retrofit2.Response;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 
 /**
  * Created by biandra on 22/09/16.
@@ -27,28 +26,22 @@ public class RefundsService {
     public static final int HTTP_500 = 500;
     private RefundApi refundApi;
     private PaymentConverter paymentConverter;
-    private ErrorConverter errorConverter;
 
-    private RefundsService(RefundApi refundApi, PaymentConverter paymentConverter, ErrorConverter errorConverter){
+    private RefundsService(RefundApi refundApi, PaymentConverter paymentConverter){
         this.refundApi = refundApi;
         this.paymentConverter = paymentConverter;
-        this.errorConverter = errorConverter;
     }
 
     public static RefundsService getInstance(RefundApi refundApi) {
-        return new RefundsService(refundApi, new PaymentConverter(), new ErrorConverter());
+        return new RefundsService(refundApi, new PaymentConverter());
     }
 
     public DecidirResponse<RefundPaymentHistoryResponse> getRefunds(Long paymentId) {
         try {
             Response<RefundPaymentHistoryResponse> response = this.refundApi.getRefunds(paymentId).execute();
-            if (response.isSuccessful()) {
-                return paymentConverter.convert(response, response.body());
-            } else {
-                DecidirResponse<DecidirError> error = errorConverter.convert(response);
-                throw DecidirException.wrap(error.getStatus(), error.getMessage(), error.getResult());
-            }
-        } catch(IOException ioe) {
+
+            return this.paymentConverter.convertOrThrowSpecError(response, RefundException.class, DecidirError.class);
+        } catch(IOException | NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException ioe) {
             throw new DecidirException(HTTP_500, ioe.getMessage());
         }
     }
@@ -56,8 +49,9 @@ public class RefundsService {
     public DecidirResponse<RefundPaymentResponse> refundPayment(Long paymentId, RefundPayment refundPayment, String user) {
         try {
             Response<RefundPaymentResponse> response = this.refundApi.refundPayment(user, paymentId, refundPayment).execute();
-            return processRefundPaymentResponse(response);
-        } catch(IOException ioe) {
+
+            return this.paymentConverter.convertOrThrowSpecError(response, RefundException.class, RefundPaymentResponse.class);
+        } catch(IOException | NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException ioe) {
             throw new DecidirException(HTTP_500, ioe.getMessage());
         }
     }
@@ -65,47 +59,23 @@ public class RefundsService {
     public DecidirResponse<RefundPaymentResponse> refundMPOSPayment(Long paymentId, RefundMPOSPayment refundPayment, String user) {
         try {
             Response<RefundPaymentResponse> response = this.refundApi.refundMPOSPayment(user, paymentId, refundPayment).execute();
-            return processRefundPaymentResponse(response);
-        } catch(IOException ioe) {
+
+            return this.paymentConverter.convertOrThrowSpecError(response, RefundException.class, RefundPaymentResponse.class);
+        } catch(IOException | NoSuchMethodException | IllegalAccessException | InvocationTargetException | InstantiationException ioe) {
             throw new DecidirException(HTTP_500, ioe.getMessage());
         }
     }
 
-    private DecidirResponse<RefundPaymentResponse> processRefundPaymentResponse(Response<RefundPaymentResponse> response)
-            throws IOException, JsonParseException, JsonMappingException {
-        DecidirResponse<RefundPaymentResponse> ret = null;
+	public DecidirResponse<AnnulRefundResponse> cancelRefund(Long paymentId, Long refundId, String user) {
+		DecidirResponse<AnnulRefundResponse> result = null;
+		try {
+			Response<AnnulRefundResponse> response = this.refundApi.cancelRefund(user, paymentId, refundId).execute();
 
-        try {
-            ret = this.paymentConverter.convertOrThrowSpecError(response, RefundException.class, RefundPaymentResponse.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return ret;
-    }
+			result = this.paymentConverter.convertOrThrowSpecError(response, AnnulRefundException.class, AnnulRefundResponse.class);
+		} catch (IOException | NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException ioe) {
+			throw new DecidirException(HTTP_500, ioe.getMessage());
+		}
+		return result;
+	}
 
-    public DecidirResponse<AnnulRefundResponse> cancelRefund(Long paymentId, Long refundId, String user) {
-        DecidirResponse<AnnulRefundResponse> result = null;
-        try {
-
-            Response<AnnulRefundResponse> response = this.refundApi.cancelRefund(user, paymentId, refundId).execute();
-            result = processAnnulRefundResponse(response);
-
-        } catch(IOException ioe) {
-            throw new DecidirException(HTTP_500, ioe.getMessage());
-        } catch (NoSuchMethodException e) {
-            e.printStackTrace();
-        } catch (InstantiationException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
-    private DecidirResponse<AnnulRefundResponse> processAnnulRefundResponse(Response<AnnulRefundResponse> response)
-            throws IOException, JsonParseException, JsonMappingException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
-        return this.paymentConverter.convertOrThrowSpecError(response, AnnulRefundException.class, AnnulRefundResponse.class);
-    }
 }
