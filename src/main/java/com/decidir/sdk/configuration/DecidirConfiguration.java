304946e3c7dbcf66614c5c3809025df2e9c4da78
Juan José Mejía Rodrígue <jmejia@prismamp.com>
2018-03-23 15:03:13 -0300

    Release version 2.2

--- src/main/java/com/decidir/sdk/configuration/DecidirConfiguration.java
+++ src/main/java/com/decidir/sdk/configuration/DecidirConfiguration.java
@@ -16,7 +16,7 @@ import retrofit2.converter.jackson.JacksonConverterFactory;
  */
 public class DecidirConfiguration {
 
-    static private final String version = "2.1.5";
+    static private final String version = "2.2";
     public static final String CACHE_CONTROL = "Cache-Control";
     public static final String MAX_AGE_0 = "max-age=0";
     public static final String USER_AGENT = "User-Agent";
